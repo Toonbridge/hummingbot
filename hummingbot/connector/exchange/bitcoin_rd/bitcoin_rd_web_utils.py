a57a32a9db2bf5a5115d9b8f9b4f207de30a6e84
Juan Ortiz <juanortiz@Juans-MacBook-Air.local>
2023-02-02 22:58:25 -0400

    ascend-like update web utils

--- hummingbot/connector/exchange/bitcoin_rd/bitcoin_rd_web_utils.py
+++ hummingbot/connector/exchange/bitcoin_rd/bitcoin_rd_web_utils.py
@@ -1,57 +1,67 @@
-from typing import Callable, Optional
+import time
+from typing import Any, Dict, Optional
 
-import hummingbot.connector.exchange.bitcoin_rd.bitcoin_rd_constants as CONSTANTS
-from hummingbot.connector.time_synchronizer import TimeSynchronizer
-from hummingbot.connector.utils import TimeSynchronizerRESTPreProcessor
+from hummingbot.connector.exchange.bitcoin_rd import bitcoin_rd_constants as CONSTANTS
 from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
 from hummingbot.core.web_assistant.auth import AuthBase
-from hummingbot.core.web_assistant.connections.data_types import RESTMethod
+from hummingbot.core.web_assistant.connections.data_types import RESTRequest
+from hummingbot.core.web_assistant.rest_pre_processors import RESTPreProcessorBase
 from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
+
+
+class BitcoinRDRESTPreProcessor(RESTPreProcessorBase):
+    async def pre_process(self, request: RESTRequest) -> RESTRequest:
+        if request.headers is None:
+            request.headers = {}
+        # Generates generic headers required by BitcoinRD
+        headers_generic = {}
+        headers_generic["Accept"] = "application/json"
+        headers_generic["Content-Type"] = "application/json"
+        # Headers signature to identify user as an HB liquidity provider.
+        request.headers = dict(
+            list(request.headers.items()) + list(headers_generic.items()) + list(get_hb_id_headers().items())
+        )
+        return request
+
+
+def get_hb_id_headers() -> Dict[str, Any]:
+    """
+    Headers signature to identify user as an HB liquidity provider.
+
+    :return: a custom HB signature header
+    """
+    return {
+        "request-source": "hummingbot-liq-mining",
+    }
 
 
 def public_rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> str:
     """
-    Creates a full URL for provided public REST endpoint
-    :param path_url: a public REST endpoint
-    :param domain: the BitcoinRD domain to connect to ("com" or "us"). The default value is "com"
+    Creates a full URL for provided private REST endpoint
+    :param path_url: a private REST endpoint
+    :param domain: domain to connect to
     :return: the full URL to the endpoint
     """
-    return CONSTANTS.REST_URL.format(domain) + CONSTANTS.PUBLIC_API_VERSION + path_url
+    return CONSTANTS.PUBLIC_REST_URL + path_url
 
 
 def private_rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> str:
     """
     Creates a full URL for provided private REST endpoint
     :param path_url: a private REST endpoint
-    :param domain: the BitcoinRD domain to connect to ("com" or "us"). The default value is "com"
-    :return: the full URL to the endpoint
+    :param domain: the domain to connect to
+    :return: None, we use overwrite_url instead
     """
-    return CONSTANTS.REST_URL.format(domain) + CONSTANTS.PRIVATE_API_VERSION + path_url
+    return CONSTANTS.PRIVATE_REST_URL + path_url
 
 
 def build_api_factory(
-        throttler: Optional[AsyncThrottler] = None,
-        time_synchronizer: Optional[TimeSynchronizer] = None,
-        domain: str = CONSTANTS.DEFAULT_DOMAIN,
-        time_provider: Optional[Callable] = None,
-        auth: Optional[AuthBase] = None, ) -> WebAssistantsFactory:
+    throttler: Optional[AsyncThrottler] = None,
+    domain: str = CONSTANTS.DEFAULT_DOMAIN,
+    auth: Optional[AuthBase] = None,
+) -> WebAssistantsFactory:
     throttler = throttler or create_throttler()
-    time_synchronizer = time_synchronizer or TimeSynchronizer()
-    time_provider = time_provider or (lambda: get_current_server_time(
-        throttler=throttler,
-        domain=domain,
-    ))
-    api_factory = WebAssistantsFactory(
-        throttler=throttler,
-        auth=auth,
-        rest_pre_processors=[
-            TimeSynchronizerRESTPreProcessor(synchronizer=time_synchronizer, time_provider=time_provider),
-        ])
-    return api_factory
-
-
-def build_api_factory_without_time_synchronizer_pre_processor(throttler: AsyncThrottler) -> WebAssistantsFactory:
-    api_factory = WebAssistantsFactory(throttler=throttler)
+    api_factory = WebAssistantsFactory(throttler=throttler, auth=auth, rest_pre_processors=[BitcoinRDRESTPreProcessor()])
     return api_factory
 
 
@@ -60,16 +70,7 @@ def create_throttler() -> AsyncThrottler:
 
 
 async def get_current_server_time(
-        throttler: Optional[AsyncThrottler] = None,
-        domain: str = CONSTANTS.DEFAULT_DOMAIN,
-) -> float:
-    throttler = throttler or create_throttler()
-    api_factory = build_api_factory_without_time_synchronizer_pre_processor(throttler=throttler)
-    rest_assistant = await api_factory.get_rest_assistant()
-    response = await rest_assistant.execute_request(
-        url=public_rest_url(path_url=CONSTANTS.SERVER_TIME_PATH_URL, domain=domain),
-        method=RESTMethod.GET,
-        throttler_limit_id=CONSTANTS.SERVER_TIME_PATH_URL,
-    )
-    server_time = response["serverTime"]
-    return server_time
+    throttler: Optional[AsyncThrottler] = None,
+    domain: str = CONSTANTS.DEFAULT_DOMAIN,
+) -> int:
+    return int(time.time() * 1e3)
