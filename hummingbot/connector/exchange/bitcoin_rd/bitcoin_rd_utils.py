8c9f84eec1a77412f7d88e08fea1f1c613830a9e
Kael Abbott <kael0227@gmail.com>
2023-03-02 20:33:04 -0400

    update utils'

--- hummingbot/connector/exchange/bitcoin_rd/bitcoin_rd_utils.py
+++ hummingbot/connector/exchange/bitcoin_rd/bitcoin_rd_utils.py
@@ -21,7 +21,7 @@ def is_exchange_information_valid(exchange_info: Dict[str, Any]) -> bool:
     :param exchange_info: the exchange information for a trading pair
     :return: True if the trading pair is enabled, False otherwise
     """
-    return exchange_info.get("status", None) == "TRADING" and "SPOT" in exchange_info.get("permissions", list())
+    return True
 
 def _time():
     """
